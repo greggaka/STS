48029694cee13d12ec0430c0ce27a90008d7af0b
greggaka <greggakamine@gmail.com>
2022-12-05 20:54:47 -1000

    added Path Variables to daikichi

--- daikichi-routes/src/main/java/com/gregg/daikichiroutes/controllers/DaikichiController.java
+++ daikichi-routes/src/main/java/com/gregg/daikichiroutes/controllers/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.gregg.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,21 @@ public class DaikichiController {
 	@GetMapping("/tomorrow")
 	public String daikichiTomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/m/travel/{city}")
+	public String daikichiTravel (@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	
+	@GetMapping("/m/lotto/{num}")
+	public String daikichiLotto(@PathVariable("num") Integer num) {
+		if (num%2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor, but now is a great time to spend time with family and friends.";
+		}
 	}
 
 }
